464ea1cd8034b96afa1aef371aee3d021c29e899
Akshat Gupta <akshat2gupta@gmail.com>
2025-05-21 06:29:52 +0530

    Fix data_over_time function for Render deployment

--- helper.py
+++ helper.py
@@ -42,10 +42,9 @@ def country_year_list(df):
     return years,country
 
 def data_over_time(df,col):
-
-    nations_over_time = df.drop_duplicates(['Year', col])['Year'].value_counts().reset_index().sort_values('index')
+    nations_over_time = df.drop_duplicates(['Year', col])['Year'].value_counts().reset_index()
     nations_over_time.rename(columns={'index': 'Edition', 'Year': col}, inplace=True)
-    return nations_over_time
+    return nations_over_time.sort_values('Edition')
 
 
 def most_successful(df, sport):
